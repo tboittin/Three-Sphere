528cc108795bc8b7ee038e67365166cc81e291e5
tboittin <53752390+tboittin@users.noreply.github.com>
2023-09-12 17:13:48 +0200

    Sphere rotation

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import * as THREE from 'three';
-
+// Threejs
 let scene, camera, renderer, sphere;
 
 function init() {
@@ -20,9 +19,47 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById('three-output').appendChild(renderer.domElement);
 
+  // texture
+  const canvas = document.createElement('canvas');
+  canvas.width = 512;
+  canvas.height = 512;
+  const ctx = canvas.getContext('2d');
+
+  const tailleCase = 64; // La taille de chaque case du damier
+  const nbCases = canvas.width / tailleCase;
+
+  for (let i = 0; i < nbCases; i++) {
+    for (let j = 0; j < nbCases; j++) {
+      if ((i + j) % 2 === 0) {
+        ctx.fillStyle = 'white';
+      } else {
+        ctx.fillStyle = 'black';
+      }
+      ctx.fillRect(i * tailleCase, j * tailleCase, tailleCase, tailleCase);
+    }
+  }
+
+  const texture = new THREE.CanvasTexture(canvas);
+
   // sphere
   const geometry = new THREE.SphereGeometry(1, 32, 32);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const material = new THREE.MeshBasicMaterial({ map: texture });
   sphere = new THREE.Mesh(geometry, material);
   scene.add(sphere);
+
+  renderer.render(scene, camera);
 }
+
+// rotation
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+//appels
+init();
+animate();
